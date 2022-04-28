afe0a22054571afa3835dbc1db099ae348ba2e79
YohanToledo <yohan.gabriel.bernart@gmail.com>
2022-04-28 01:48:45 -0300

    Users Table

--- frontend/src/components/Users/index.tsx
+++ frontend/src/components/Users/index.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import axios, { AxiosRequestConfig } from 'axios';
+import { Link} from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { BASE_URL } from 'requests';
+import 'index.css';
 
 
 
@@ -18,6 +20,7 @@ function Users() {
     
     function updateData(){
         setUpdate(true);
+        alert("tabela atualizada");
     }
 
     useEffect(() => {
@@ -46,7 +49,7 @@ function Users() {
         */
 
         const config: AxiosRequestConfig = {
-            baseURL: BASE_URL,
+            baseURL: "http://186.237.57.196:8080",
             method: 'PUT',
             url: '/users/save',
             data: {
@@ -58,34 +61,72 @@ function Users() {
 
         axios(config).then(response => {
             console.log(response.data);
+            window.location.replace("/users/");
         });
 
     }
     
     return (
         <div>
-            <h1>Users</h1>
-            <ul>
-                    {users.map((user: { name: string; email: string; }) => (
-                        <li>{"NAME:  " + user.name + " --- EMAIL: " + user.email}</li> 
-                    ))}
-            </ul>
-            <button className="btn btn-primary" onClick={updateData}> update </button>
-
             <form onSubmit={handleSubmit}>
+                <div className="save-user">
+                    <div className="col-md-2 ">
+                        <h3>Cadastrar usuario</h3>
+                    </div>
+                    <div className="row">
+                        
+                        <div className="col-md-2">
+                            <input id="name" className="form-control" type="text" placeholder="Name"/>
+                        </div>
+                        <div className="col-md-2">
+                            <input id="email" className="form-control" type="text" placeholder="Email"/>
+                        </div>
+                        <div className="col-md-1">
+                            <button type="submit" className="btn btn-primary mt-1"> save </button>
+                        </div>
+                        <div className="col-md-2">
+                            <Link to="/">
+                                <button className="btn btn-secondary mt-1">Voltar</button>
+                            </Link>
+                        </div>   
+                    </div>
+                </div>
+                <br/>
+            </form>
+            
+
+            <div className="container custom-container">
                 <div className="row">
                     <div className="col-md-2">
-                        <input id="name" className="form-control" type="text" placeholder="Name"/>
+                        <h1>Users</h1>
                     </div>
                     <div className="col-md-2">
-                        <input id="email" className="form-control" type="text" placeholder="Email"/>
+                        <button className="btn btn-primary mt-2" onClick={updateData}> update </button>
                     </div>
-                    <div className="col-md-2">
-                        <button type="submit" className="btn btn-primary col-md-6"> save </button>
-                    </div>   
                     
                 </div>
-            </form>
+                <div className="table-box col-md-6">
+                    <table className="table table-striped table-sm">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nome</th>
+                                <th>Email</th>
+                                
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {users.map((user: { id: number; name: string; email: string; }) => (
+                            <tr key={user.id}>
+                                <td className="width15">{user.id}</td> 
+                                <td >{user.name}</td> 
+                                <td >{user.email}</td> 
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>              
         </div>
 
     );
